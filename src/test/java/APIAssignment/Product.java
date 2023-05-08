947863c0ec2973b2009eaa160a31040d9f80ba90
Tejashri Ghogare <tejashrig@synerzipune.local>
2023-05-08 17:57:23 +0530

    Added class

--- src/test/java/APIAssignment/Product.java
+++ src/test/java/APIAssignment/Product.java
@@ -1,2 +1,15 @@
-package APIAssignment;public class Product {
+package APIAssignment;
+
+public class Product {
+    public String productId;
+    public String productName;
+    public String productPrice;
+    public String productBrand;
+
+    public Product(String productListId, String productListName, String productListPrice, String productListBrand) {
+        productId = productListId;
+        productName = productListName;
+        productPrice = productListPrice;
+        productBrand = productListBrand;
+    }
 }
