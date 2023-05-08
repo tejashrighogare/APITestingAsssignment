8e50d9ec339b9bd3aaba9669afa20255cd7cff77
Tejashri Ghogare <tejashrig@synerzipune.local>
2023-05-08 17:56:16 +0530

    Removed .getName() method and used lambda function

--- src/test/java/APIAssignment/APIAssignmentGet.java
+++ src/test/java/APIAssignment/APIAssignmentGet.java
@@ -7,21 +7,21 @@ import io.restassured.response.*;
 
 import static io.restassured.RestAssured.*;
 
-import java.util.logging.*;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.*;
 
-import org.apache.log4j.*;
+import org.slf4j.*;
+import org.slf4j.Logger;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
 public class APIAssignmentGet {
+
     private String productListURL = "";
-    private Logger logger = Logger.getLogger(APIAssignmentGet.class.getName());
+    private Logger logger = LoggerFactory.getLogger(APIAssignmentGet.class);
 
     @Test(priority = 1)
     public void validateStatusCode() {
-        productListURL = ConfigReader.getURL("baseURL");
+        productListURL = ConfigReader.getPropertyValue("baseURL");
         Response responseVariable = get(productListURL);
         var statusCodeValue = responseVariable.getStatusCode();
         Assert.assertEquals(200, statusCodeValue);
@@ -33,39 +33,48 @@ public class APIAssignmentGet {
 
     @Test(priority = 2)
     public void validateList() {
-        productListURL = ConfigReader.getURL("baseURL");
+        productListURL = ConfigReader.getPropertyValue("baseURL");
         var getALLProductList = given().when().get(productListURL).then().log().all().toString();
         logger.info("List return by API is" + getALLProductList);
     }
 
     @Test(priority = 3)
     public void validateContent() {
-        productListURL = ConfigReader.getURL("baseURL");
+        productListURL = ConfigReader.getPropertyValue("baseURL");
         Response response = RestAssured.given().when().get(productListURL);
         JsonPath jsonResponse = new JsonPath(response.asString());
         logger.info("my json response: " + jsonResponse.get("products"));
-        var productsArray = jsonResponse.get("products");
-        var idLength = jsonResponse.getInt("products.id.size()");
-        var productName = ConfigReader.getURL("productName");
-        var price = ConfigReader.getURL("price");
-        var brand = ConfigReader.getURL("brand");
-        var category = ConfigReader.getURL("category");
-        var currentProductName = "";
-        for (var counter = 0; counter < productsArray.toString().length(); counter++) {
-            currentProductName = jsonResponse.getString("products[" + counter + "].name");
-            if (currentProductName.contentEquals("Men Tshirt")) {
-                Assert.assertEquals(currentProductName, productName);
-                Assert.assertEquals(jsonResponse.getString("products[" + counter + "].price"), price);
-                Assert.assertEquals(jsonResponse.getString("products[" + counter + "].brand"), brand);
-                Assert.assertEquals(jsonResponse.getString("products[" + counter + "].category.category"), category);
-                break;
-            }
+        List<Product> productsArray = jsonResponse.get("products");
+        List<Product> getProductData = new ArrayList<>();
+        var productId = ConfigReader.getPropertyValue("productId");
+        var productName = ConfigReader.getPropertyValue("productName");
+        var productPrice = ConfigReader.getPropertyValue("price");
+        var productBand = ConfigReader.getPropertyValue("brand");
+        Product objectProduct = new Product(productId, productName, productPrice, productBand);
+        getProductData.add(objectProduct);
+        for (int index = 0; index < productsArray.size(); index++) {
+            String id = jsonResponse.getString("products[" + index + "].id");
+            String name = jsonResponse.getString("products[" + index + "].name");
+            String price = jsonResponse.getString("products[" + index + "].price");
+            String brand = jsonResponse.getString("products[" + index + "].brand");
+            getProductData.forEach(displayList -> {
+                if (name.equals(displayList.productName)) {
+                    logger.info(displayList.productId);
+                    logger.info(displayList.productName);
+                    logger.info(displayList.productBrand);
+                    logger.info(displayList.productPrice);
+                    Assert.assertEquals(displayList.productId, id);
+                    Assert.assertEquals(displayList.productName, name);
+                    Assert.assertEquals(displayList.productPrice, price);
+                    Assert.assertEquals(displayList.productBrand, brand);
+                }
+            });
         }
     }
 
     @Test(priority = 4)
     public void validateLength() {
-        var productListURL = ConfigReader.getURL("baseURL");
+        var productListURL = ConfigReader.getPropertyValue("baseURL");
         var response = given()
                 .when().get(productListURL).then().extract().asString();
         JsonPath jsonResponse = new JsonPath(response);
