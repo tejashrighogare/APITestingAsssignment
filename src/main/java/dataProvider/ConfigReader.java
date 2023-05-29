1ec23d45a853d9744f95d403858e5982613ef9ed
Tejashri Ghogare <tejashrig@synerzipune.local>
2023-05-29 15:48:34 +0530

    Added logger and user specific expression in catch

--- src/main/java/dataProvider/ConfigReader.java
+++ src/main/java/dataProvider/ConfigReader.java
@@ -1,12 +1,14 @@
 package dataProvider;
 
-import org.apache.log4j.*;
+import org.slf4j.*;
+import org.slf4j.Logger;
 
 import java.io.*;
 import java.util.Properties;
 
 public class ConfigReader {
     private static Properties propertiesObject;
+    private static Logger logger = LoggerFactory.getLogger(ConfigReader.class);
 
     public static String getPropertyValue(String propertyName) {
         String configFilePath = System.getProperty("user.dir");
@@ -16,11 +18,13 @@ public class ConfigReader {
 
     public static Properties readPropertiesFile(String fileName) {
         try {
+
             FileInputStream fileStream = new FileInputStream(fileName);
             propertiesObject = new Properties();
             propertiesObject.load(fileStream);
-        } catch (Exception e) {
-            e.printStackTrace();
+        } catch (Exception FileNotFoundException) {
+
+            logger.info("File not found exception occurred");
         }
         return propertiesObject;
     }
